90f05c90101a99a07fd2dda804404434a0c4d241
Mahmoud Elshamy <shamyyoun@gmail.com>
2017-01-16 03:44:05 +0200

    Logout action has been added to main activity.

--- app/src/main/java/com/twsela/client/activities/MainActivity.java
+++ app/src/main/java/com/twsela/client/activities/MainActivity.java
@@ -1,12 +1,16 @@
 package com.twsela.client.activities;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
 import android.view.MenuItem;
 
 import com.twsela.client.R;
+import com.twsela.client.controllers.ActiveUserController;
 import com.twsela.client.fragments.HomeFragment;
+import com.twsela.client.utils.DialogUtils;
 
 public class MainActivity extends ParentActivity {
     private static int DRAWER_GRAVITY = Gravity.LEFT;
@@ -18,7 +22,10 @@ public class MainActivity extends ParentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // customize toolbar
         setToolbarIcon(R.drawable.menu_icon);
+        createOptionsMenu(R.menu.menu_main);
 
         // init drawer layout
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -50,6 +57,10 @@ public class MainActivity extends ParentActivity {
                 onMenuIcon();
                 return true;
 
+            case R.id.action_logout:
+                onLogout();
+                return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -65,6 +76,27 @@ public class MainActivity extends ParentActivity {
 
     public void closeMenuDrawer() {
         drawerLayout.closeDrawer(DRAWER_GRAVITY);
+    }
+
+    private void onLogout() {
+        // show confirm dialog
+        DialogUtils.showConfirmDialog(this, R.string.logout_q, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                logout();
+            }
+        }, null);
+    }
+
+    private void logout() {
+        // logout in active user controller
+        ActiveUserController activeUserController = new ActiveUserController(this);
+        activeUserController.logout();
+
+        // goto splash activity
+        Intent splashIntent = new Intent(this, SplashActivity.class);
+        startActivity(splashIntent);
+        finish();
     }
 
     @Override
